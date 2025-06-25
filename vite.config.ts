ebaf19c07d191228c7bc95817227bc66e74d1075
htakkala89 <htakkala@blitz.gg>
2025-06-24 17:04:30 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@shared": path.resolve(__dirname, "../shared"),
-      "@assets": path.resolve(__dirname, "../attached_assets"),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
+  root: "client",
   build: {
     outDir: "dist",
     emptyOutDir: true,
